8ea294d954a0d4297ab7a639017dc4576f9bafa7
C Dhivya Bharathi <67786951+CDhivyaBharathi@users.noreply.github.com>
2021-01-29 21:28:22 +0530

    Password: qwerty
    This is an automation file that clicks a pic and saves it to our pc when we enter the wrong password

---
+++ Automation.py
@@ -0,0 +1,42 @@
+import cv2
+import time
+import random
+import dropbox
+
+
+startTime = time.time()
+
+def take_snapshot():
+       number = random.randint(0,100)
+    captureObject = cv2.VideoCapture(0)
+    result = True
+    while (result):
+        ret,frame = captureObject.read()
+        cv2.imwrite("NewPicture1.jpg", frame)
+        result = False
+    
+    captureObject.release()
+    cv2.destroyAllWindows()
+
+
+def main():
+    password = 'qwerty'
+    userPassword = input('Enter the password : ')
+    if (userPassword!=password):
+      print("Incorrect password")
+      take_snapshot()
+    
+    if (userPassword == password):
+        print("Password is correct")
+
+   
+
+
+
+main() 
+
+
+        
+
+
+ 
